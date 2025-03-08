db30e6975bc7de7c23f0aa7075335828495ff264
mens1s <ahmet_yigit_02@hotmail.com>
2025-03-08 13:24:22 +0300

    MediateR publish methods done

--- src/Modules/Catalog/Catalog/CatalogModule.cs
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.Data.Interceptors;
 
 namespace Catalog
 {
@@ -8,12 +10,34 @@ namespace Catalog
     {
         public static IServiceCollection AddCatalogModule(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
+
             // Add services to the container.
+            var connectionString = configuration.GetConnectionString("Database");
+
+            services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+            services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+            services.AddDbContext<CatalogDbContext>((sp, options) =>
+            {
+                options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+                options.UseNpgsql(connectionString);
+            });
+
+            services.AddScoped<IDataSeeder, CatalogDataSeeder>();
+
             return services;
         }
 
         public static IApplicationBuilder UseCatalogModule(this IApplicationBuilder app)
         {
+
+            // Use Data - Infrastructure services
+            app.UseMigration<CatalogDbContext>();
+
             return app;
         }
     }
---
+++ src/Modules/Catalog/Catalog/Data/Seed/InitialData.cs
@@ -0,0 +1,13 @@
+﻿namespace Catalog.Data.Seed;
+public static class InitialData
+{
+    public static IEnumerable<Product> Products =>
+        new List<Product>
+        {
+            Product.Create(Guid.NewGuid(), "Iphone 15", new List<string> { "Telephone" }, "Apple new iphone a15", "product1.jpg", 100),
+            Product.Create(Guid.NewGuid(), "Samsung Galaxy", new List<string> { "Telephone" }, "Samsung new galaxy s22", "product2.jpg", 200),
+            Product.Create(Guid.NewGuid(), "Xiaomi Redmi", new List<string> { "Telephone" }, "Xiaomi new redmi note 11", "product3.jpg", 300),
+            Product.Create(Guid.NewGuid(), "OnePlus", new List<string> { "Computer" }, "OnePlus new oneplus 10", "product4.jpg", 400),
+        };
+}
+
